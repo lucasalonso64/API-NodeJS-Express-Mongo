d6f9f7069789f33b6b5da0de9009a6047d60d491
Lucas <alonsosistemas@gmail.com>
2019-09-24 17:39:44 -0300

    inicial1

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require('../database');
+const mongoose = require('../../database');
 const bcrypt = require('bcryptjs')
 const UserSchema = new mongoose.Schema({
     name: {
@@ -15,6 +15,16 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+        select: false,
+    },
+
+    passwordResetToken: {
+        type: String,
+        select: false,
+    },
+
+    passwordResetExpires: {
+        type: Date,
         select: false,
     },
 
